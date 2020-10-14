5581d61613bc08aea3b67946f7dc11b73571409e
Lifanova <iramilka123@gmail.com>
2020-10-14 21:05:30 +0300

    DI with setter, from properties and using list(DI)

--- SpringFirst/src/main/java/org/example/MusicPlayer.java
+++ SpringFirst/src/main/java/org/example/MusicPlayer.java
@@ -1,14 +1,40 @@
 package org.example;
 
+import java.util.List;
+
 public class MusicPlayer {
     private Music music;
+    private List<Music> musicList;
+
+    private String name;
+    private int volume;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getVolume() { return volume; }
+    public void setVolume(int volume) { this.volume = volume; }
+
+    public List<Music> getMusicList() { return musicList; }
+    public void setMusicList(List<Music> musicList) { this.musicList = musicList; }
 
     // IoC
     public MusicPlayer (Music music) {
         this.music = music;
     }
+    // Пустой конструктор для внедрения зависимости через setter
+    public MusicPlayer () {}
+
+    public void setMusic (Music music) {
+        this.music = music;
+    }
 
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
+    }
+    public void playList() {
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
--- SpringFirst/src/main/java/org/example/TestSpring.java
+++ SpringFirst/src/main/java/org/example/TestSpring.java
@@ -6,10 +6,18 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        Music music = context.getBean("musicBean", Music.class);
+      //  Music music = context.getBean("musicBean", Music.class);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+       // MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        System.out.println("================Homework=================");
+        MusicPlayer musicPlayerList = context.getBean("musicPlayerList", MusicPlayer.class);
+        musicPlayerList.playList();
+
         context.close();
     }
 }
