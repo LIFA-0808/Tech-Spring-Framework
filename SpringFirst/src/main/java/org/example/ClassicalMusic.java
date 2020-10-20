10ca7468867cea576019ed588192224df68004cd
Lifanova <iramilka123@gmail.com>
2020-10-20 15:23:19 +0300

    Add init-method, destroy-method, factory-method to .xml

--- SpringFirst/src/main/java/org/example/ClassicalMusic.java
+++ SpringFirst/src/main/java/org/example/ClassicalMusic.java
@@ -1,6 +1,20 @@
 package org.example;
 
 public class ClassicalMusic implements Music{
+    private ClassicalMusic () {}
+
+    public static ClassicalMusic getClassicalMusic () {
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit () {
+        System.out.println("Doing my Initialization");
+    }
+
+    public void doMyDestroy () {
+        System.out.println("Doing my Destruction");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
